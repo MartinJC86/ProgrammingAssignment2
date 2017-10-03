3dbc7c8cc8b0dd4233b7fbae3fb745d9cc2ebde6
MartinJC86 <yashuar@hotmail.com>
2017-10-03 16:46:42 +0200

    Commit Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+Matrix_coursera : matrix(1:9, nrow = 3, ncol = 3)
+inverse <- matrix() ## free variable 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## function which use inverse variable 
+cacheSolve <- function( inverse, original) {
+  if (identical(inverse , original)) { # if not compute then compute
+    cash <- inverse
+  }else {
+    tmp <- makeCacheMatrix(Caa) ## call function to inverse matrix.
+    cash <- tmp$inverse
+  }
+  cash
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(ztmp = matrix()) { 
+  ## take Matrix in argument and add another function which get the original or inverse the matrix
+  ztmp = ztmp
+  list(
+    get = function()ztmp,
+    inverse = function()solve(ztmp, tol = 1e-20)
+  )
 }
